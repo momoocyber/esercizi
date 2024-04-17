7691d63c8f4b36232619f8c66fa520613e865546
momoocyber <med.morjani123@outlook.it>
2024-04-17 13:52:42 +0200

    File lezione2.py aggiunto

---
+++ lezione2/lezione2.py
@@ -0,0 +1,13 @@
+# Mohamed Morjani
+# 17/04/2024
+
+print("hello world")
+
+# 2-3. Personal Message: Use a variable to represent a person’s
+# name, and print a message to that person. 
+# Your message should be simple, such as,
+# “Hello Eric, would you like to learn some Python today?”
+
+name: str = "Mario"
+message: str= f"Ciao {name}, ti va di imparare un po di python insieme"
+print(message)
